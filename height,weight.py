1f1947f715a0ba3986cca809735f76efe33ec9b5
Mahizhanpan <135017893+mahizhanpan@users.noreply.github.com>
2023-06-23 13:43:19 +0530

    Add files via upload

---
+++ height,weight.py
@@ -0,0 +1,11 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> age=15
+>>> print(age)
+15
+>>> height=132
+>>> print(height)
+132
+>>> weight=32
+>>> print(weight)
+32
